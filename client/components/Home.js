f25d4289b21fb1f73cc3e235825776065dfe4d97
Sabi Kumar <skumar213@gmail.com>
2021-12-04 13:02:03 -0500

    working

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,26 +1,69 @@
-import React from 'react'
-import {connect} from 'react-redux'
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
 
 /**
  * COMPONENT
  */
 export const Home = props => {
-  const {username} = props
+  const { username } = props;
+
+  const [current, setCurrent] = useState(null)
+  const [result, setResult] = useState(null)
+
+  const ricksAndSticks = {
+    1: "rick1.jpeg",
+    2: "rick2.jpeg",
+    3: "rick3.jpeg",
+    4: "rick4.jpeg",
+    5: "stick1.webp",
+    6: "stick2.jpg",
+    7: "stick3.jpeg",
+    8: "stick4.jpeg",
+  };
+
+  const index = () => Math.floor(Math.random() * (9 - 1) + 1);
+
+  useEffect(() => {
+    setCurrent(ricksAndSticks[index()])
+  }, [])
+
+  const handleClick = (evt) => {
+    if (current.includes(evt.target.value)) {
+      setResult("Good Job!!")
+
+      setTimeout(() => {
+        setResult(null)
+        setCurrent(ricksAndSticks[index()])
+      }, 3000)
+    } else {
+      setResult("Try Again!!")
+    }
+  }
 
   return (
     <div>
-      <h3>Welcome, {username}</h3>
+      <h2>Welcome, {username}</h2>
+      <h3>Rick or Stick!!</h3>
+
+      <img src={current} />
+      <div>
+        <button value ='rick' onClick={handleClick}>Rick</button>
+        <button value ='stick' onClick={handleClick}>Stick</button>
+      </div>
+      <div>
+        {result ? <div>{result}</div> : null}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 /**
  * CONTAINER
  */
 const mapState = state => {
   return {
-    username: state.auth.username
-  }
-}
+    username: state.auth.username,
+  };
+};
 
-export default connect(mapState)(Home)
+export default connect(mapState)(Home);
